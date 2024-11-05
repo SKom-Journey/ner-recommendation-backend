73c5c8a96e7c8acaf4a1e801a97f1b4b1379f35c
Melvin-Jovano <melvinjovano2@gmail.com>
2024-11-05 20:39:48 +0700

    feat: qrs

--- controllers/create_qr_controller.py
+++ controllers/create_qr_controller.py
@@ -1,9 +1,12 @@
 from utils.response import response
-from services.qrs import create_qr
-from models.QR import QR
+from services.qrs import create_qr, qr
 
 def create_qr_controller(json: dict):
+    checkIfExist = qr(json['table_number'])
+
+    if checkIfExist != None:
+        return response("Table Number Already Exist", "ERROR", True)
+
     return response(create_qr(
-        json['table_number'],
-        json['is_enabled']
+        json['table_number']
     ))
--- services/qrs.py
+++ services/qrs.py
@@ -8,20 +8,20 @@ def delete_qr(id: str):
     delete = db.get_collection(tb_name).delete_one({"_id": ObjectId(id)})
     return delete.deleted_count 
 
-def create_qr(table_number: str, is_enabled: bool):
-    qr_id = db.get_collection(tb_name).insert_one({"table_number": table_number, "is_enabled": is_enabled}).inserted_id
+def create_qr(table_number: str):
+    qr_id = db.get_collection(tb_name).insert_one({"table_number": table_number}).inserted_id
     qr = db.get_collection(tb_name).find_one({"_id": ObjectId(qr_id)})
     return QR(
         id=str(qr['_id']),
-        is_enabled=qr['is_enabled'],
         table_number=qr['table_number'],
     ).model_dump() 
  
 def qr(table_number: str):
     qr = db.get_collection(tb_name).find_one({"table_number": table_number})
+    if qr == None:
+        return None
     return QR(
         id=str(qr['_id']),
-        is_enabled=qr['is_enabled'],
         table_number=qr['table_number'],
     ).model_dump() 
 
@@ -31,7 +31,6 @@ def qrs():
         result.append(
             QR(
                 id=str(qr['_id']),
-                is_enabled=qr['is_enabled'],
                 table_number=qr['table_number'],
             )
             .model_dump()
